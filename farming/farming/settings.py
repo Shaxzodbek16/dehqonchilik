7ac34a380027810609e2385f5ad4b74e7caa83ff
Shaxzodbek <134439368+Shaxzodbek16@users.noreply.github.com>
2024-07-01 23:12:17 +0500

    Update settings.py

--- farming/farming/settings.py
+++ farming/farming/settings.py
@@ -110,6 +110,6 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/var/www/media/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
